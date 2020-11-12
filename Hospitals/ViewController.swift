0a5cbc124aa95b166f7d6753e2dd2649e07cc9d6
cocoaken <ken_tabb@me.com>
2020-11-12 20:17:40 +0000

    Initial Commit

---
+++ Hospitals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hospitals
+//
+//  Created by ken on 12/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
